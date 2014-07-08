5df130d773e7962ba1e41101253bd65e3bc54f86
Rajesh Balamohan <rbalamohan@apache.org>
2014-07-08 17:04:54 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,58 @@
+Installation (One time setup):
+=============================
+
+1. mvn install:install-file -DgroupId=com.fifesoft -DartifactId=rsyntaxtextarea -Dversion=2.5.1 -Dpackaging=jar -Dfile=./rsyntaxtextarea-2.5.1.jar
+
+2. Download apache-jmeter-2.11.zip and extract to a folder
+
+3. Edit $JMETER_HOME/bin/jmeter and comment out the following line
+
+java $ARGS $JVM_ARGS -jar "`dirname "$0"`/ApacheJMeter.jar" "$@"
+
+4. Add relevant Hadoop/Hive classpath based on your installation.
+
+JMETER_HOME=/grid/0/rajesh/jmeter/apache-jmeter-2.11/
+HADOOP_HOME=/grid/0/hadoop
+HADOOP_CONF_DIR=/grid/0/hadoopConf
+TEZ_HOME=/grid/0/rajesh/tez-autobuild/dist/tez
+TEZ_CONF_DIR=/grid/0/rajesh/tez-autobuild/dist/tez/conf
+HIVE_HOME=/grid/0/rajesh/tez-autobuild/dist/hive
+HIVE_CONF_DIR=/grid/0/rajesh/tez-autobuild/dist/hive/conf
+
+CLASSPATH=$CLASSPATH:$JMETER_HOME/bin/*:$JMETER_HOME/lib/*:$JMETER_HOME/lib/ext/*:$HADOOP_CONF_DIR
+:$HADOOP_HOME
+/share/hadoop/common/*:$HADOOP_HOME/share/hadoop/common/lib/*:$HIVE_HOME/lib/*:$HIVE_CONF_DIR
+:$TEZ_HOME/*:$TEZ_HOME/lib/*:$TEZ_CONF_DIR
+
+echo $CLASSPATH
+#java $ARGS $JVM_ARGS -jar "`dirname "$0"`/ApacheJMeter.jar" "$@"
+java $ARGS $JVM_ARGS -cp $CLASSPATH org.apache.jmeter.NewDriver "$@"
+
+5. Build the project using "mvn clean build"
+
+6. Copy ./target/hive-jdbc-sampler-1.0-SNAPSHOT.jar to $JMETER_HOME/lib/ext folder
+
+Starting HiveServer2 with custom port
+======================================
+1. hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10001
+
+Running jmeter in non-gui mode:
+==============================
+
+1. cd $JMETER_HOME/bin
+
+2. Modify same hive.jmx attached here and change based on your needs (edit the script in GUI mode
+ in desktop which is lot easier)
+
+3. To run in non-GUI mode, ./jmeter -n -t hive.jmx
+
+4. Pass any hiveconf related items in "additionalConnectionParams" parameter of hive.jmx (e.g ?hive.execution.engine=tez;hive.auto.convert.join.noconditionaltask=false)
+
+
+Important Note:
+==============
+1. "query" argument in jmeter JMX script accepts only one query.  Do not add multiple queries in the same argument (e.g "use mydb; select count(*) from test" is invalid)
+
+Debugging:
+=========
+1. In case hive job is not starting, check the console logs of hiveserver2 (to verify if the query is getting submitted by jmeter).  Check jmeter.log as well for any exceptions.
---
+++ src/main/java/org/apache/hive/jdbc/HiveJDBCSampler.java
@@ -0,0 +1,144 @@
+package org.apache.hive.jdbc;
+
+import java.io.Serializable;
+import java.sql.*;
+import org.apache.jorphan.collections.Data;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+
+public class HiveJDBCSampler extends AbstractJavaSamplerClient implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private boolean driverLoaded = false;
+  Connection conn = null;
+  String url;
+  String username;
+  String password;
+  boolean select; //select statement or update statement
+  JavaSamplerContext context;
+
+  @Override
+  public Arguments getDefaultParameters() {
+    Arguments defaultParameters = new Arguments();
+    defaultParameters.addArgument("url", "jdbc:hive2://cn041:10000/default");
+    defaultParameters.addArgument("driver", "org.apache.hive.jdbc.HiveDriver");
+    defaultParameters.addArgument("username", "root");
+    defaultParameters.addArgument("password", "");
+    defaultParameters.addArgument("additionalConnectionParams", "?hive.execution.engine=tez");
+    defaultParameters.addArgument("Select_OR_Update", "select");
+    defaultParameters.addArgument("initialization_query", "drop table result");
+    return defaultParameters;
+  }
+
+
+  private Data getDataFromResultSet(ResultSet rs) throws SQLException {
+    ResultSetMetaData meta = rs.getMetaData();
+    //TODO: if there is too much data, this might throw OOM in jmeter
+    //Easier option: rewrite your query to persist large data in temp table
+    Data data = new Data();
+    int numColumns = meta.getColumnCount();
+    String[] dbCols = new String[numColumns];
+    for (int i = 0; i < numColumns; i++) {
+      dbCols[i] = meta.getColumnName(i + 1);
+      data.addHeader(dbCols[i]);
+    }
+    while (rs.next()) {
+      data.next();
+      for (int i = 0; i < numColumns; i++) {
+        Object o = rs.getObject(i + 1);
+        if (o instanceof byte[]) {
+          o = new String((byte[]) o);
+        }
+        data.addColumnValue(dbCols[i], o);
+      }
+    }
+    rs.close();
+    return data;
+  }
+
+  @Override
+  public void setupTest(JavaSamplerContext context) {
+    try {
+      this.context = context;
+      this.url = context.getParameter("url")
+          + context.getParameter("additionalConnectionParams");
+      this.password = context.getParameter("username");
+      this.username = context.getParameter("password");
+      this.select = context.getParameter("Select_OR_Update", "select").trim().equalsIgnoreCase
+          ("select");
+      initConnection();
+    } catch(Exception e) {
+      getLogger().error(e.getMessage());
+    }
+  }
+
+  private void initConnection()
+      throws Exception {
+    if (!driverLoaded) {
+      try {
+        Class.forName(context.getParameter("driver"));
+        System.out.println("Loaded driver : " + context.getParameter("driver"));
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+        System.exit(1);
+      }
+      driverLoaded = true;
+    }
+    if (conn == null) {
+      conn = DriverManager.getConnection(this.url, this.username, this.password);
+      System.out.println("Got connection");
+    }
+  }
+
+  private void closeConnection(Connection conn) {
+    if (conn != null) {
+      try {
+        conn.close();
+        System.out.println("Closing connection");
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @Override
+  public SampleResult runTest(JavaSamplerContext context) {
+    Statement stmt = null;
+    SampleResult result = new SampleResult();
+    try {
+      result.sampleStart();
+      stmt = conn.createStatement();
+      if (select) {
+        ResultSet rs = stmt.executeQuery(context.getParameter("query"));
+        Data data = getDataFromResultSet(rs);
+        result.setResponseMessage("\nSuccess : " + data.toString());
+      } else {
+        int rows = stmt.executeUpdate(context.getParameter("query"));
+        result.setResponseMessage("\nSuccess : " + rows);
+      }
+      result.sampleEnd();
+      result.setSuccessful(true);
+    } catch(Exception e) {
+      result.sampleEnd();
+      result.setSuccessful( false );
+      result.setResponseMessage( "Error: " + e );
+
+      java.io.StringWriter stringWriter = new java.io.StringWriter();
+      e.printStackTrace( new java.io.PrintWriter( stringWriter ) );
+      result.setResponseData(stringWriter.toString());
+      result.setDataType(org.apache.jmeter.samplers.SampleResult.TEXT);
+    }
+    return result;
+  }
+
+  @Override
+  public void teardownTest(JavaSamplerContext context) {
+    try {
+      closeConnection(conn);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
